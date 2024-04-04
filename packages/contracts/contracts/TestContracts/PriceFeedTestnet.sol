1b9206e1c4062b7ceffef7ff00f9fce76464bf3f
prajwalghate <prajwalghate18@gmail.com>
2024-04-04 16:50:51 +0530

    oracle update

--- packages/contracts/contracts/TestContracts/PriceFeedTestnet.sol
+++ packages/contracts/contracts/TestContracts/PriceFeedTestnet.sol
@@ -4,31 +4,51 @@ pragma solidity 0.6.11;
 
 import "../Interfaces/IPriceFeed.sol";
 import "../Dependencies/Ownable.sol";
+import "../Dependencies/IPriceRouter.sol";
+import "../Dependencies/CheckContract.sol";
+
 
 /*
  * PriceFeed placeholder for testnet and development. The price is simply set manually and saved in a state
  * variable. The contract does not connect to a live Chainlink price feed.
  */
-contract PriceFeedTestnet is IPriceFeed, Ownable {
-    uint256 private _price = 200 * 1e18;
+contract PriceFeedTestnet is IPriceFeed,CheckContract, Ownable {
+    uint public lastGoodPrice;
+    IPriceRouter public priceRouter;  // Mainnet Chainlink aggregator
+
+     function setAddresses(
+        address _priceRouterAddress
+    )
+        external
+        onlyOwner
+    {
+        checkContract(_priceRouterAddress);
+        priceRouter = IPriceRouter(_priceRouterAddress);
+        _renounceOwnership();
+    }
 
     // --- Functions ---
 
     // View price getter for simplicity in tests
     function getPrice() external view returns (uint256) {
-        return _price;
+        return lastGoodPrice;
     }
 
     function fetchPrice() external override returns (uint256) {
-        // Fire an event just like the mainnet version would.
-        // This lets the subgraph rely on events to get the latest price even when developing locally.
-        emit LastGoodPriceUpdated(_price);
-        return _price;
+        //Fetch the price and store it 
+        uint price=priceRouter.getPrice();
+        _storePrice(price);
+        return price;
     }
 
-    // Manual external price setter.
-    function setPrice(uint256 price) external onlyOwner returns (bool) {
-        _price = price;
+    function _storePrice(uint _currentPrice) internal {
+        lastGoodPrice = _currentPrice;
+        emit LastGoodPriceUpdated(_currentPrice);
+    }
+
+    function setPrice(uint256 price) external returns (bool) {
+        // _price = price;
         return true;
     }
+
 }
